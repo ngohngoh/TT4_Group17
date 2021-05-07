869fbb9e06a32a64153ec0b7c4f5097e0d41e7b1
lauxx1997 <52653970+lauxx1997@users.noreply.github.com>
2021-05-07 14:00:01 +0800

    Add files via upload

---
+++ ewallet/src/api/userTransaction.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL:
+    "https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view",
+  headers: {
+    "x-api-key": "PDprxfeN6h773d3G9RvBN2zp6H79R0tjp17Fhd0d",
+  },
+});
