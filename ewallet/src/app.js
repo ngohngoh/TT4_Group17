15c916ae9a6d4fc5d245d1e2cab0dcea11e44475
ShawnTay996 <tay.shawn@u.nus.edu>
2021-05-07 14:21:54 +0800

    Added Login

--- ewallet/src/app.js
+++ ewallet/src/app.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import AccountView from './AccountView';
 import transactionView from './transactionView';
+import Login from './Login';
+
 
 class App extends React.Component {
   render() {
     return (
       //<div><AccountView /></div>
-      <div><transactionView /></div>
+      
+      <div>
+        <Login /> 
+        <transactionView />
+      
+      </div>
     );
   }
 }
